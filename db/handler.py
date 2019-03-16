2e29aa5717c83ff305bfc8053fe89d3c4fd77d5d
Igor Tsallagov <igor.tsallagov@gmail.com>
2019-03-16 23:17:29 +0300

    Updated methods for handling database

--- db/handler.py
+++ db/handler.py
@@ -1,7 +1,8 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 from db.db_config import HOST, PORT, RES_DB, RES_COL
 
 
+# Returns the number of documents in the collection
 def count_documents():
     with MongoClient(HOST, PORT) as client:
         db = client[RES_DB]
@@ -10,6 +11,7 @@ def count_documents():
         return query
 
 
+# Returns information about the user with user_id
 def find_user(user_id):
     with MongoClient(HOST, PORT) as client:
         db = client[RES_DB]
@@ -20,18 +22,32 @@ def find_user(user_id):
         return query
 
 
-def find_all():
+# Deletes user with user_id from database
+def find_delete_user(user_id):
     with MongoClient(HOST, PORT) as client:
         db = client[RES_DB]
         collection = db[RES_COL]
-        query = collection.find()
+        query = collection.find_one_and_delete(
+            {'id': user_id}
+        )
         return query
 
 
+# Returns number of last inserted documents
+def get_documents(number):
+    with MongoClient(HOST, PORT) as client:
+        db = client[RES_DB]
+        collection = db[RES_COL]
+        query = collection.find(
+            sort=[('_id', DESCENDING)], limit=number
+        )
+        return query
+
+
+# Returns the list of indexes
 def get_indexes():
     with MongoClient(HOST, PORT) as client:
         db = client[RES_DB]
         collection = db[RES_COL]
         query = collection.get_indexes()
         return query
-
